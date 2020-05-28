0bdce05927be851556bd15c69e8d5d09c39429c7
Naeem <naeempatel2019@u.northwestern.edu>
2020-05-28 17:47:56 -0500

    initial parsing

--- App.py
+++ App.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template, request, redirect, url_for
 import csv
+import pandas as pd
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return render_template("index.html")
+def index():
+
+    states = pd.read_csv('/Users/naeem/Desktop/Covid19/predictions.csv', nrows=0)
+    state_name = []
+    print(states)
+
+    for row in states:
+        state_name.append(row)
+
+    return render_template("index.html", state_name=state_name)
 
 
 if __name__ == "__main__":
